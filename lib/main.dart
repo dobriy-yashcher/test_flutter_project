91b79fa08778c7dc0929e98a8f6c543e773a374e
dobriy-yashcher <golovinsergey17@gmail.com>
2023-03-03 19:53:46 +0300

    edit main color

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:test_project/pages/auth_page.dart';
 
 void main() {
-  runApp(const AppThemeMaterial());
+  runApp(const MainApp());
 }
 
-class AppThemeMaterial extends StatelessWidget {
-  const AppThemeMaterial({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AuthPage(),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.amber),
       debugShowCheckedModeBanner: false,
+      home: AuthPage(),
     );
   }
 }
